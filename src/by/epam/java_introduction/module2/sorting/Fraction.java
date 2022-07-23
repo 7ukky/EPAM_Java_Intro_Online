cfee3a6908c11516b52234d961078695e571548e
7ukky <rdv.5.07.2002@gmail.com>
2022-07-24 01:39:40 +0300

    Класс для решения задачи 8

---
+++ src/by/epam/java_introduction/module2/sorting/Fraction.java
@@ -0,0 +1,15 @@
+package by.epam.java_introduction.module2.sorting;
+
+public class Fraction {
+
+    int numerator;
+    int denominator;
+
+    Fraction(int numerator, int denominator) {
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+
+    }
+
+}
